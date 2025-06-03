74ff6efc6c63e231d8f916ab86e2b2ae407fbf91
ElChavoDelBronx <20243ds087@utez.edu.mx>
2025-06-02 21:29:45 -0600

    Corrección de formula para calcular perimetro en getter de Perimetro

--- App.java
+++ App.java
@@ -8,7 +8,7 @@ public class App {
         FigureAbs sq = new Square(10);
         FigureAbs cir = new Circle(10);
         FigureAbs triangle = new Triangle(20, 25, 20);
-        FigureAbs tRectangulo = new TriangleRect(20, 10, 30);
+        FigureAbs tRectangulo = new TriangleRect(20, 10, 10, 20, 30);
         FigureAbs tEquilatero = new TriangleEqui(20, 20, 20);
 
 
