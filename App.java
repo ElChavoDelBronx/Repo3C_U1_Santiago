3e99ef6e861b13b9f3e94b4f0a11557e3449538d
ElChavoDelBronx <20243ds087@utez.edu.mx>
2025-06-02 21:12:42 -0600

    U2_TI_01_FigurasAbstraccion

---
+++ App.java
@@ -0,0 +1,25 @@
+import Model.*;
+
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        FigureAbs sq = new Square(10);
+        FigureAbs cir = new Circle(10);
+        FigureAbs triangle = new Triangle(20, 25, 20);
+        FigureAbs tRectangulo = new TriangleRect(20, 10, 30);
+        FigureAbs tEquilatero = new TriangleEqui(20, 20, 20);
+
+
+        FigureAbs[] figures = { sq, cir, triangle, tRectangulo, tEquilatero};
+
+        for (FigureAbs figureAbs : figures) {
+            System.out.println("La clase es: " + figureAbs.getClass().getSimpleName());
+            System.out.println("El area es: " + figureAbs.getArea());
+            System.out.println("El perimetro es: " + figureAbs.getPerimeter());
+            System.out.println("----------------------------------------------------");
+        }
+
+    }
+}
